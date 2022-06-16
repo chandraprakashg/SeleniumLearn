002261e13944f046970554bfa2ea6de859f96f29
chandraprakashg <95429023+chandraprakashg@users.noreply.github.com>
2022-06-16 12:05:26 +0530

    Initial commit

---
+++ OpenGooglepage.java
@@ -0,0 +1,20 @@
+package seleniumlearn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGooglepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedrive\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://www.google.co.in");
+		//To google the w3schools by press Enter key
+		driver.findElement(By.name("q")).sendKeys("w3schools"+Keys.ENTER);
+	}
+
+}
+
